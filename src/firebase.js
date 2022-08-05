336e0613db405792eadbb123a94a7a9115a09498
PhYell <94902895+PhYell@users.noreply.github.com>
2022-08-05 23:56:09 +0400

    added item page

--- src/firebase.js
+++ src/firebase.js
@@ -115,6 +115,16 @@ export const getShopPageContent = async () => {
     // return allItems;
 };
 
+export const getCartItems = async (id) => {
+    const docRef = doc(db, "products", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        return docSnap.data();
+    }
+    return null;
+};
+
 // export const getCartItems = async (user) => {
 //     const docRef = doc(db, "users", user.uid);
 //     const docSnap = await getDoc(docRef);
--- src/pages/item.page/Item.page.jsx
+++ src/pages/item.page/Item.page.jsx
@@ -1,9 +1,37 @@
+import "./item.scss";
+
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+import { getCartItems } from "../../firebase";
 
 const ItemPage = () => {
     const { id } = useParams();
 
-    return <h1>{id}</h1>;
+    const [item, setItems] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getCartItems(id)
+            .then((res) => setItems(res))
+            .catch((err) => setError(err))
+            .finally(setLoading(false));
+        console.log("123");
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>error</p>;
+
+    return (
+        <main>
+            <img src={item?.imageUrl} alt={item?.name} className="item-image" />
+            <div className="item-text">
+                <h3 className="item-name">{item?.name}</h3>
+                <p className="item-price">{item?.price}$</p>
+            </div>
+        </main>
+    );
 };
 
 export default ItemPage;
